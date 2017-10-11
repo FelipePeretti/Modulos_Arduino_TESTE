e852033ea933c404fa4ca8ca43bd0ad5b4c09588
felipe <felipeperetti_rve@hotmail.com>
2017-10-11 15:26:55 -0300

    up

--- Sketchs/Modulo_RTC-DS3231__F_P__10_10_2017/Modulo_RTC-DS3231__F_P__10_10_2017.ino
+++ Sketchs/Modulo_RTC-DS3231__F_P__10_10_2017/Modulo_RTC-DS3231__F_P__10_10_2017.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+  // Arduino Uno/2009:
+  // ----------------------
+  // DS3231:  SDA pin   -> Arduino Analog 4 or the dedicated SDA pin
+  //          SCL pin   -> Arduino Analog 5 or the dedicated SCL pin
+  //
+  // Arduino Leonardo:
 
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  #include 
 
-}
+  // Init the DS3231 using the hardware interface
+  DS3231  rtc(SDA, SCL);
+
+  void setup(){
+    // Setup Serial connection
+    Serial.begin(115200);
+    // Uncomment the next line if you are using an Arduino Leonardo
+    //while (!Serial) {}
+  
+    // Initialize the rtc object
+    rtc.begin();
+  
+    // The following lines can be uncommented to set the date and time
+    //rtc.setDOW(WEDNESDAY);     // Set Day-of-Week to SUNDAY
+    //rtc.setTime(12, 0, 0);     // Set the time to 12:00:00 (24hr format)
+    //rtc.setDate(1, 1, 2014);   // Set the date to January 1st, 2014
+  }
+
+  void loop(){
+    // Send Day-of-Week
+    Serial.print(rtc.getDOWStr());
+    Serial.print(" ");
+  
+    // Send date
+    Serial.print(rtc.getDateStr());
+    Serial.print(" -- ");
+
+    // Send time
+    Serial.println(rtc.getTimeStr());
+  
+    // Wait one second before repeating :)
+    delay (1000);
+  }
